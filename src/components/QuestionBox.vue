09e62d9dd437f9b3d8714bf3d4597f6c41764cbb
Fran Martinez <martinezcoder@gmail.com>
2019-08-02 11:14:12 +0200

    add answers

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,9 +7,11 @@
 
       <hr class="my-4">
 
-      <p>
-        List of answers
-      </p>
+      <b-list-group>
+        <b-list-group-item v-for="(answer, index) in answers" :key="index">
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">
@@ -24,6 +26,22 @@ export default {
   props: {
     currentQuestion: Object,
     next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
   }
 }
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
